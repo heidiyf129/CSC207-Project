a2c5ed65ad164a1adbca9e8e7f26fff0722a7d17
John Zhang <clearsky21@MacBook-Pro-2020.local>
2023-09-30 14:39:33 -0400

    应该没问题了

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,6 @@ public class Main {
         RequestBody body = RequestBody.create(mediaType, "");
         Request request = new Request.Builder()
                 .url("https://api.airvisual.com/v2/nearest_city?key=788d7141-476d-41bd-8acd-17a3f9ff5d55")
-                .method("GET", body)
                 .build();
         Response response = client.newCall(request).execute();
         System.out.println(response.body().string());
