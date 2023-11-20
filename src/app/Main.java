b189abfc493d4e862f2db64a6dc9ddf779c31cd5
heidiyf <heidiyf29@gmail.com>
2023-11-20 18:44:06 -0500

    heidi user branch

--- src/app/Main.java
+++ src/app/Main.java
@@ -58,12 +58,12 @@ public class Main {
     private void displaySignInView() {
         JFrame signInFrame = new JFrame("Sign In");
         signInFrame.setSize(400, 400);
-        signInFrame.setLayout(new GridBagLayout()); // Set layout to GridBagLayout
+        signInFrame.setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.insets = new Insets(4, 4, 4, 4); // Provide some spacing
+        gbc.insets = new Insets(4, 4, 4, 4);
 
         JTextField usernameField = new JTextField(15);
         JTextField cityField = new JTextField(15);
@@ -80,8 +80,8 @@ public class Main {
 
             if (!username.isEmpty() && !city.isEmpty() && !state.isEmpty() && !country.isEmpty()) {
                 storeUserInfo(username, city, state, country);
-                signInFrame.dispose(); // Close the sign-in window
-                displayLoginView(); // Open the login window
+                signInFrame.dispose();
+                displayLoginView();
             } else {
                 JOptionPane.showMessageDialog(signInFrame, "Please fill in all fields.",
                         "Incomplete Form", JOptionPane.ERROR_MESSAGE);
@@ -101,7 +101,7 @@ public class Main {
         gbc.fill = GridBagConstraints.NONE;
         signInFrame.add(submitButton, gbc);
 
-        signInFrame.setLocationRelativeTo(null);  // Center the sign-in window
+        signInFrame.setLocationRelativeTo(null);
         signInFrame.setVisible(true);
     }
 
@@ -125,10 +125,9 @@ public class Main {
         loginButton.addActionListener(e -> {
             String username = usernameField.getText().trim();
             String[] location = getUserLocation(username);
-            if (location != null && location.length == 3) { // Ensure location has 3 elements
-                loginFrame.dispose(); // Close the login window
+            if (location != null && location.length == 3) {
+                loginFrame.dispose();
 
-                // Set the text fields with the user's location
                 countryTextField.setText(location[2]);
                 stateTextField.setText(location[1]);
                 cityTextField.setText(location[0]);
@@ -145,7 +144,7 @@ public class Main {
         loginFrame.add(usernameField);
         loginFrame.add(loginButton);
 
-        loginFrame.setLocationRelativeTo(null);  // Center the login window
+        loginFrame.setLocationRelativeTo(null);
         loginFrame.setVisible(true);
     }
 
@@ -172,9 +171,6 @@ public class Main {
 
         JPanel panel = new JPanel(new GridLayout(0, 2)); // Use GridLayout for an ordered layout
 
-
-
-        // Initialize the text fields if they haven't been already
         if (countryTextField == null) {
             countryTextField = new JTextField(15);
         }
@@ -185,12 +181,11 @@ public class Main {
             cityTextField = new JTextField(15);
         }
 
-        // Set the text fields with the user's location
         countryTextField.setText(country);
         stateTextField.setText(state);
         cityTextField.setText(city);
 
-        // Add components to the panel
+
         panel.add(new JLabel("Country:"));
         panel.add(countryTextField);
 
@@ -200,16 +195,16 @@ public class Main {
         panel.add(new JLabel("City:"));
         panel.add(cityTextField);
 
-        // Initialize the button if it hasn't been already
+
         if (checkAqiButton == null) {
             checkAqiButton = new JButton("Check AQI");
         }
 
-        // Result label to display the AQI
+
         JLabel resultLabel = new JLabel("AQI: ");
         panel.add(resultLabel);
 
-        // Text area for detailed analysis
+
         JTextArea analysisArea = new JTextArea(5, 50);
         analysisArea.setEditable(false);
         panel.add(new JScrollPane(analysisArea));
@@ -218,31 +213,29 @@ public class Main {
             checkAqiButton.removeActionListener(al);
         }
 
-        // Add the button to the panel and define its action
+
         panel.add(checkAqiButton);
         checkAqiButton.addActionListener(e -> {
-            // The action to perform when the Check AQI button is clicked
+
             try {
                 String aqiStr = fetchAQI(cityTextField.getText().trim(), stateTextField.getText().trim(), countryTextField.getText().trim());
-                int aqiValue = Integer.parseInt(aqiStr); // Parse AQI value from string
+                int aqiValue = Integer.parseInt(aqiStr);
                 resultLabel.setText("AQI for " + cityTextField.getText().trim() + ": " + aqiValue);
 
                 AirQuality airQuality = new AirQuality(aqiValue);
-                AnalyzeAirQuality analyzer = new AnalyzeAirQuality(); // The analyzer instance
+                AnalyzeAirQuality analyzer = new AnalyzeAirQuality();
                 String analysis = analyzer.analyze(airQuality);
-                analysisArea.setText(analysis); // Set analyzed result to the TextArea
+                analysisArea.setText(analysis);
             } catch (NumberFormatException ex) {
                 resultLabel.setText("Error: Invalid AQI value received.");
                 analysisArea.setText("");
             } catch (Exception ex) {
-                // Handle unexpected errors
                 resultLabel.setText("An error occurred while fetching AQI.");
                 analysisArea.setText("");
-                ex.printStackTrace(); // This will print the error details to the console
+                ex.printStackTrace();
             }
         });
 
-        // Add the panel to the main frame
         mainFrame.add(panel, BorderLayout.CENTER);
         mainFrame.setSize(600, 400);
 
@@ -250,39 +243,32 @@ public class Main {
         mainFrame.setLocationRelativeTo(null); // Center the window
         mainFrame.setVisible(true);
 
-        // Automatically click the Check AQI button to fetch the data
         SwingUtilities.invokeLater(() -> checkAqiButton.doClick());
-        // Add the panel to the main frame
         mainFrame.add(panel, BorderLayout.CENTER);
 
-        // Add a Back button at the bottom
+
         JButton backButton = new JButton("Back to Main");
         backButton.addActionListener(e -> {
-            // Clear the content of the main frame
+
             mainFrame.getContentPane().removeAll();
             mainFrame.setLayout(new FlowLayout());
 
-            // Resize the main frame before redisplaying the main view
-            mainFrame.setSize(300, 100); // Resize the main frame to 300x100
+            mainFrame.setSize(300, 100);
 
-            // Redisplay the main view
             displayMainView();
 
-            // Revalidate and repaint the main frame
             mainFrame.revalidate();
             mainFrame.repaint();
 
-            // Ensure the main frame is centered
+
             mainFrame.setLocationRelativeTo(null);
         });
 
-// Add the Back button to the main frame, at the bottom
         mainFrame.add(backButton, BorderLayout.PAGE_END);
 
     }
 
     private String fetchAQI(String city, String state, String country) {
-        // Create an instance of your AirQualityController
         AirQualityController airQualityController = new AirQualityController(new APIAdapter());
         return airQualityController.fetchAirQuality(city, state, country);
     }
